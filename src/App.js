6eff413e8b15d484efbdca7ed78d82de39a07416
mohammedsanta <mohammedsantaa@gmail.com>
2023-07-13 10:50:43 +0300

    find

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './home/Home';
-// import { Route, Routes } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AllEpisodes from './home/allEpisode/AllEpisodes';
 import About from './home/about/About';
 
 function App() {
   return (
     <>
-    
-    <Router basename={process.env.PUBLIC_URL}>
+
+    <div className='app'>
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/episodes' element={<AllEpisodes />} />
         <Route path='/about' element={<About />} />
       </Routes>
-    </Router>
+    
+    </div>
     
     </>
   );
